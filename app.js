0d6582680824e5809295bf5ad0fed7970c69c0f0
Jessie Katz <86683237+jessiekatz@users.noreply.github.com>
2024-04-25 12:52:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const http = require('http');
 const connStr = "mongodb+srv://newuser:123@mycluster.brnb7re.mongodb.net/?retryWrites=true&w=majority";
 const port = process.env.PORT || 3000;
 
-console.log("This goes to the console window");
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<h2>Hello World</h2>");
-  res.write ("Success!  This app is deployed online");
-  console.log('hey');
 
   MongoClient.connect(connStr, function(err, db) {
     if (err) {
@@ -32,4 +25,12 @@ http.createServer(function (req, res) {
       });
     }
   });
+
+console.log("This goes to the console window");
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<h2>Hello World</h2>");
+  res.write ("Success!  This app is deployed online");
+  console.log('hey');
 }).listen(port);
