06758bd5f7dae5fe404c1b3d70522b98801201bb
Jessie Katz <86683237+jessiekatz@users.noreply.github.com>
2024-04-25 12:33:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
-const connStr= "mongodb+srv://newuser:123@mycluster.brnb7re.mongodb.net/?retryWrites=true&w=majority"
-	var http = require('http');
-var port = process.env.PORT || 3000;
+const http = require('http');
+
+const connStr = "mongodb+srv://newuser:123@mycluster.brnb7re.mongodb.net/?retryWrites=true&w=majority";
+const port = process.env.PORT || 3000;
+
 console.log("This goes to the console window");
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("<h2>Hello World</h2>");
   res.write ("Success!  This app is deployed online");
-  console.log('hey')
+  console.log('hey');
+
   MongoClient.connect(connStr, function(err, db) {
-    
-  if(err) { console.log(err); }
-  else {
-	  var dbo = db.db("library");
-	  var collection = dbo.collection('books');
-	  res.write(collection.findOne());
-	  db.close();
+    if (err) {
+      console.log(err);
+      res.end(); // End response if there's an error
+    } else {
+      var dbo = db.db("library");
+      var collection = dbo.collection('books');
+      
+      collection.findOne({}, function(err, result) {
+        if (err) {
+          console.log(err);
+          res.end(); // End response if there's an error
+        } else {
+          res.write(JSON.stringify(result)); // Assuming result is a JSON object
+          db.close();
+          res.end(); // End response after sending data
+        }
+      });
     }
   });
-	
-   res.end();
 }).listen(port);
-
-
