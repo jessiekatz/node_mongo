c21caa8951ee7abcdb39b2004ebca5e9b317e3b5
Jessie Katz <86683237+jessiekatz@users.noreply.github.com>
2024-04-25 13:32:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write("<h2>Hello World</h2>");
+   res.write("<h2>Hello World...</h2>");
 const MongoClient = require('mongodb').MongoClient;
 const connStr= "mongodb+srv://newuser:1234@mycluster.brnb7re.mongodb.net/?retryWrites=true&w=majority"
 	  
 console.log('hey')
   MongoClient.connect(connStr, function(err, db) {
+	console.log('after connect')
     
   if(err) { console.log(err); }
   else {
     var dbo = db.db("library");
 	  var collection = dbo.collection('books');
     console.log("Success!");
-	  res.write('connected');
-	  db.close();
+	//  res.write('connected');
+	 // db.close();
   }//end else
 });//end connect
 	res.end();
