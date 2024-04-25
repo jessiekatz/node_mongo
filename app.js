92f9b7434d48277f63139d018afa5ee932da1fd2
Jessie Katz <86683237+jessiekatz@users.noreply.github.com>
2024-04-25 13:01:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const connStr= "mongodb+srv://newuser:123@cluster0.rjmq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const connStr= "mongodb+srv://newuser:1234@cluster0.rjmq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 	  
 console.log('hey')
   MongoClient.connect(connStr, function(err, db) {
